6db4d7bff7b1d44a081fb7c230939580e7ef7c57
thomasjbotelho <thomasjbotelho@gmail.com>
2022-07-21 22:35:01 -0300

    optimizations

--- src/lib.rs
+++ src/lib.rs
@@ -62,45 +62,54 @@ impl Vec2D {
 }
 
 #[wasm_bindgen]
-#[derive(Clone, Debug, Default, PartialEq, Serialize, Deserialize)]
-pub struct Particle {
-    prev_pos: Vec2D,
-    curr_pos: Vec2D,
-    mass: f32,
-    constraints: Vec<Constraint>,
-    forces: Vec2D,
+#[derive(Clone, Debug, Default, Serialize, Deserialize)]
+pub struct Particles {
+    prev_pos: Vec<Vec2D>,
+    curr_pos: Vec<Vec2D>,
+    mass: Vec<f32>,
+    #[serde(skip_serializing)]
+    constraints: Vec<Vec<Constraint>>,
+    forces: Vec<Vec2D>,
 }
 
-impl Particle {
-    pub fn new(curr_pos: Vec2D, prev_pos: Vec2D, mass: f32) -> Particle {
-        Particle {
+impl Particles {
+    pub fn new(curr_pos: Vec<Vec2D>, prev_pos: Vec<Vec2D>, mass: Vec<f32>) -> Particles {
+        Particles {
             prev_pos,
             curr_pos,
             mass,
             constraints: Vec::new(),
-            forces: Vec2D::new(0.0, 0.0),
+            forces: Vec::new(),
         }
     }
 
-    pub fn add_constraint(&mut self, c: Constraint) {
-        self.constraints.push(c);
+    pub fn add(&mut self, prev_pos: Vec2D, curr_pos: Vec2D, mass: f32) {
+        self.prev_pos.push(prev_pos);
+        self.curr_pos.push(curr_pos);
+        self.mass.push(mass);
+        self.constraints.push(Vec::new());
+        self.forces.push(Vec2D::zero());
     }
 
-    pub fn remove_constraint(&mut self, c: Constraint) {
-        let index = self.constraints.iter().position(|i| *i == c);
+    pub fn add_constraint(&mut self, ind: usize, c: Constraint) {
+        self.constraints[ind].push(c);
+    }
+
+    pub fn remove_constraint(&mut self, ind: usize, c: Constraint) {
+        let index = self.constraints[ind].iter().position(|i| *i == c);
         if let Some(idx) = index {
             self.constraints.remove(idx);
         } else {
-            log!("Attempting to remove non-existent constraint.");
+            //log!("Attempting to remove non-existent constraint.");
         }
     }
 
-    pub fn add_force(&mut self, f: Vec2D) {
-        self.forces.add(f);
+    pub fn add_force(&mut self, ind: usize, f: Vec2D) {
+        self.forces[ind].add(f);
     }
 
-    pub fn annul_forces(&mut self) {
-        self.forces = Vec2D::new(0.0, 0.0);
+    pub fn annul_forces(&mut self, ind: usize) {
+        self.forces[ind] = Vec2D::new(0.0, 0.0);
     }
 }
 
@@ -115,14 +124,17 @@ pub enum Constraint {
 pub struct Scene {
     width: u32,
     height: u32,
-    particles: Vec<Particle>,
+    particles: Particles,
     timestep: f32,
 }
 
 #[wasm_bindgen]
 impl Scene {
     pub fn new(width: u32, height: u32) -> Scene {
-        let particles = Vec::new();
+        let mass = Vec::new();
+        let curr_pos = Vec::new();
+        let prev_pos = Vec::new();
+        let particles = Particles::new(curr_pos, prev_pos, mass);
         // let mut constraints = Vec::new();
         // constraints.push(Constraint::ToFixed {
         //     fixed: Vec2D { x: 0, y: 0 },
@@ -131,7 +143,40 @@ impl Scene {
             width,
             height,
             particles,
-            timestep: 1.0 / 60.0,
+            timestep: 1. / 15.,
+        }
+    }
+
+    pub fn init_rope(&mut self) {
+        let gravity = Vec2D::new(0., 10.);
+        let anchor = Vec2D::new(100., 10.);
+
+        let seg_len: u32 = 8;
+        for i in 0..20 {
+            let x = (i * seg_len) as f32 + anchor.x;
+            let y = anchor.y;
+            let pos = Vec2D::new(x, y);
+            //let mut p = Particle::new(pos, pos, 1.);
+            self.particles.add(pos, pos, 1.);
+            self.particles.add_force(i as usize, gravity);
+
+            if i == 0 {
+                self.particles.add_constraint(
+                    i as usize,
+                    Constraint::ToFixed {
+                        fixed: anchor,
+                        dist: seg_len,
+                    },
+                );
+            } else {
+                self.particles.add_constraint(
+                    i as usize,
+                    Constraint::ToPoint {
+                        ind: (i - 1) as usize,
+                        dist: seg_len,
+                    },
+                );
+            }
         }
     }
 
@@ -147,86 +192,66 @@ impl Scene {
         JsValue::from_serde(&self.particles).unwrap()
     }
 
+    pub fn particle_positions(&self) -> *const Vec2D {
+        self.particles.curr_pos.as_ptr()
+    }
+
     pub fn step(&mut self) {
-        for p in self.particles.iter_mut() {
-            Scene::verlet_step(self.timestep, p);
+        for p in 0..self.particles.mass.len() {
+            self.verlet_step(p);
         }
 
-        for _ in 0..10 {
-            for i in 0..self.particles.len() {
+        for _ in 0..5 {
+            for i in 0..self.particles.mass.len() {
                 self.satisfy_constraints(i);
 
-                //let mut v = std::mem::replace(&mut self.particles, vec![]);
-                let mut v = std::mem::take(&mut self.particles);
-
-                let p = &mut v[i];
-                self.satisfy_constraints2(p);
-                self.particles = v;
+                // let mut v = std::mem::take(&mut self.particles);
+                // let p = &mut v[i];
+                // self.satisfy_constraints2(p);
+                // self.particles = v;
             }
         }
     }
 
-    pub fn verlet_step(timestep: f32, p: &mut Particle) {
+    pub fn verlet_step(&mut self, ind: usize) {
         // integration
-        let prev = p.prev_pos;
-        let mut curr = p.curr_pos;
-        let mut res_force = p.forces;
-        let mass = p.mass;
-        p.prev_pos = curr;
+        let prev = self.particles.prev_pos[ind];
+        let mut curr = self.particles.curr_pos[ind];
+        let mut res_force = self.particles.forces[ind];
+        let mass = self.particles.mass[ind];
+        self.particles.prev_pos[ind] = curr;
 
         // x_{i+1} = 2*x_{i} - x_{i-1} + [ (h*h) * f_i/m_i ]
-        res_force.scale(timestep * timestep / mass);
+        res_force.scale(self.timestep * self.timestep / mass);
         curr.scale(2.0);
         curr.sub(prev);
         curr.add(res_force);
-        p.curr_pos = curr;
+        self.particles.curr_pos[ind] = curr;
     }
 
-    pub fn satisfy_constraints2(&mut self, p: &mut Particle) {
-        for c in p.constraints.iter() {
-            match *c {
+    pub fn satisfy_constraints(&mut self, ind_curr: usize) {
+        //let curr_pos = self.particles.curr_pos.clone();
+        for c in 0..self.particles.constraints[ind_curr].len() {
+            match self.particles.constraints[ind_curr][c] {
                 Constraint::ToPoint { ind, dist } => {
-                    let c_pos = self.particles[ind].curr_pos;
-                    let (mut delta, d_normed) = Scene::constraint_delta(p, c_pos, dist);
+                    let (mut delta, d_normed) =
+                        self.constraint_delta(ind_curr, self.particles.curr_pos[ind], dist);
                     delta.scale(0.5 * d_normed);
-                    p.curr_pos.sub(delta);
-                    self.particles[ind].curr_pos.add(delta)
+                    self.particles.curr_pos[ind_curr].sub(delta);
+                    self.particles.curr_pos[ind].add(delta);
                 }
                 Constraint::ToFixed { fixed, dist } => {
-                    let (mut delta, d_normed) = Scene::constraint_delta(p, fixed, dist);
+                    let (mut delta, d_normed) = self.constraint_delta(ind_curr, fixed, dist);
                     delta.scale(d_normed);
-                    p.curr_pos.sub(delta);
+                    self.particles.curr_pos[ind_curr].sub(delta);
                 }
             }
         }
     }
 
-    pub fn satisfy_constraints(&mut self, ind2: usize) {
-        //let p = &mut self.particles[ind];
-        for c in 0..self.particles[ind2].constraints.len() {
-            match self.particles[ind2].constraints[c] {
-                Constraint::ToPoint { ind, dist } => {
-                    let c_pos = self.particles[ind].curr_pos;
-                    let (mut delta, d_normed) =
-                        Scene::constraint_delta(&self.particles[ind2], c_pos, dist);
-                    delta.scale(0.5 * d_normed);
-                    self.particles[ind2].curr_pos.sub(delta);
-                    //p.curr_pos.sub(delta);
-                    self.particles[ind].curr_pos.add(delta);
-                }
-                Constraint::ToFixed { fixed, dist } => {
-                    let (mut delta, d_normed) =
-                        Scene::constraint_delta(&self.particles[ind2], fixed, dist);
-                    delta.scale(d_normed);
-                    (&self.particles[ind2]).clone().curr_pos.sub(delta);
-                }
-            }
-        }
-    }
-
-    fn constraint_delta(p: &Particle, c_pos: Vec2D, c_dist: u32) -> (Vec2D, f32) {
-        let dx = p.curr_pos.x - c_pos.x;
-        let dy = p.curr_pos.y - c_pos.y;
+    fn constraint_delta(&self, ind: usize, c_pos: Vec2D, c_dist: u32) -> (Vec2D, f32) {
+        let dx = self.particles.curr_pos[ind].x - c_pos.x;
+        let dy = self.particles.curr_pos[ind].y - c_pos.y;
         //TODO: remove this using scheme in Jakobsens paper
         let d = (dx * dx + dy * dy).sqrt();
         //TODO: make sure distance is positive
