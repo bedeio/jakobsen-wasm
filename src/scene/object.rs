e5641d19e7ac3eef37335fdceb3357edc0daef52
thomasjbotelho <thomasjbotelho@gmail.com>
2022-08-12 22:49:45 -0300

    i see no changes

--- src/scene/object.rs
+++ src/scene/object.rs
@@ -1,5 +1,22 @@
-use super::{ParticleId, Vec2};
+use super::{ConstraintId, ParticleId, Vec2};
 use wasm_bindgen::prelude::*;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
-pub struct Object {}
+pub struct Object {
+    particles: Vec<ParticleId>,
+    constraints: Vec<ConstraintId>,
+}
+
+impl Object {
+    fn new() -> Object {}
+
+    fn add_particle(&self, p: ParticleId) {
+        self.particles.push(p);
+    }
+
+    fn add_constraint(&self, c: ConstraintId) {
+        self.constraints.push(c);
+    }
+
+    fn validate_polygon(&self) -> bool {}
+}
